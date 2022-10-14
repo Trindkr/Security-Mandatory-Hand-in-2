5728df98c05312b50c9ef78f0c97a2245f79b853
Trindkr <maria.trindkaer@gmail.com>
2022-10-15 00:08:50 +0200

    initial server/client setup

---
+++ Client/Alice.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	gRPC "github.com/Trindkr/Security-Mandatory-Hand-in-2-golang/proto"
+)
+
+var bob gRPC.CommitmentServiceClient
--- Server/Bob.go
+++ Server/Bob.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	
 	gRPC "github.com/Trindkr/Security-Mandatory-Hand-in-2-golang/proto"
+	//"google.golang.org/grpc"
 )
 
 type Bob struct {
 	gRPC.UnimplementedCommitmentServiceServer
 }
 
-// func (s *Server) ValidateCommitment(ctx context.Context, commitment *Commitment) (*gRPC.Commitment_Res, error) {
-    
-//     //ack :=  // make an instance of your return type
-//     return (ack, nil)
-// }
+func main(){
+
+}
+
+
+
